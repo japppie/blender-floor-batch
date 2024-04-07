df8985feb93a40913dedeed08cec18ecb71fa784
japppie <131072659+japppie@users.noreply.github.com>
2024-04-07 14:05:25 +0200

    Script now takes data from CSV and renders every 'found' SKU to a unique file

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Short description
 Set up a render queue for multiple floors in Blender
-Using Floor Generator 2.0 by NodeCrafted https://blendermarket.com/products/floor-generator (you require a license to use the file, so it is excluded from this repo)
+Using Floor Generator 2.0 by NodeCrafted https://blendermarket.com/products/floor-generator (you require a license to use the file, go buy it at Blender Market. The use of this script is free.)
 
 Script imports CSV
 Writes all floor data into instance of Floordata class
-Changes all textures in the floor generator
+Changes all textures and tile size in the floor generator
 Renders 'MAIN_CAMERA'
 
+# BEFORE YOU RUN THIS SCRIPT
+Make sure to make the following changes in the Floor Generator 2.0 Blendfile:
+- Floor object should be called 'floor'
+- Floor material should be called 'floor_material'
+- Render camera should be called 'MAIN CAMERA'
+- The 9 TEXTURE NODES in floor material should be called **tex00, tex01, tex02 ... tex08**
+
 # TO DO
-Use data from Floordata class to change settings in floor generator.
-Iterate over floordata_list to render multiple images
+Further clean-up
+Possibly create separate settings file
+Implement patterns and grouts
--- utils/floordata.py
+++ utils/floordata.py
@@ -1,8 +1,34 @@
+import os
+
 class Floordata():
-    def __init__(self, sku, size, pattern, planks, grout, suffix):
+    def __init__(self, sku, size, pattern, grout, suffix, image_location):
         self.sku = sku
-        self.size = size
+        self.size_x, self.size_y = self.extract_size(size)
         self.pattern = pattern
-        self.planks = planks
         self.grout = grout
         self.suffix = suffix
+        self.filenames = []
+        self.texture_count = self.count_textures(image_location)  # Automatically count textures
+
+    def extract_size(self, size):
+        size = size.replace(' x ', 'x').replace(',', '.')
+        size_parts = size.split('x')
+        if len(size_parts) == 2:
+            try:
+                size_x = float(size_parts[0].strip())
+                size_y = float(size_parts[1].strip())
+                return size_x, size_y
+            except ValueError:
+                print("Invalid number format:", size)
+                return None, None
+        else:
+            return None, None
+
+    def count_textures(self, image_location):
+        texture_count = 0
+        for file in os.listdir(image_location):
+            if self.sku in file:
+                texture_count += 1
+                self.filenames.append(file)
+                print(f"Found texture: {file}")
+        return int(texture_count)
